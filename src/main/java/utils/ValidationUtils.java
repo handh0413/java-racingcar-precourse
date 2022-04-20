f80fce9474b1cdd27860e0bffbdd8a0858b067a2
Donghee Han <handh0413@naver.com>
2022-04-21 01:56:56 +0900

    refactor: ValidationUtils 리팩토링
    상수 값을 이용한 리팩토링

--- src/main/java/utils/ValidationUtils.java
+++ src/main/java/utils/ValidationUtils.java
@@ -1,8 +1,11 @@
 package utils;
 
+import domain.Constant;
+
 public class ValidationUtils {
 
     public static boolean validateMovementCondition(int number) {
-        return 0 <= number && number <= 9;
+        return Constant.MOVEMENT_STOP_CONDITION_MIN <= number
+                && number <= Constant.MOVEMENT_STOP_CONDITION_MAX;
     }
 }
