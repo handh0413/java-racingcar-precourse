4fbd332cbed6c4a4a7b85588fb54b01a20c08660
Donghee Han <handh0413@naver.com>
2022-04-23 21:37:33 +0900

    feat: RacingController 기능 추가
      - 가장 많이 이동한 자동차의 거리를 확인한다.
      - 경주에서 우승한 자동차의 정보를 가져온다.

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -4,6 +4,9 @@ import domain.Car;
 import domain.RacingCars;
 import domain.RacingCount;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameController {
     private RacingCars racingCars;
     private RacingCount racingCount;
@@ -17,6 +20,20 @@ public class GameController {
         for (int i = 0; i < racingCount.getCount(); i++) {
             racingCars.drive();
         }
+    }
+
+    public List<Car> findWinners() {
+        int highest = findWinnersPosition();
+        return getRacingCars().getCars().stream()
+                .filter(car -> car.getPosition() == highest)
+                .collect(Collectors.toList());
+    }
+
+    public int findWinnersPosition() {
+        return getRacingCars().getCars().stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(-1);
     }
 
     public Car findRacingCar(String name) {
