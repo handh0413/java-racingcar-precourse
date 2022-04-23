dd54143be8fc9b9540b566fe1e795cfb0249d905
Donghee Han <handh0413@naver.com>
2022-04-24 01:59:41 +0900

    feat: 경주 게임 실행 결과 보여주는 기능

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,8 @@
 package view;
 
+import domain.Car;
+import domain.RacingCars;
+
 public class OutputView {
     private static final String ERROR_PREFIX = "[ERROR]";
 
@@ -9,5 +12,28 @@ public class OutputView {
 
     public static void printErrorMessage(String message) {
         System.out.println(ERROR_PREFIX + " " + message);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void printLine() {
+        System.out.println();
+    }
+
+    public static void printRacingResult(RacingCars rc) {
+        for (Car car : rc.getCars()) {
+            printCarResult(car);
+        }
+        System.out.println();
+    }
+
+    private static void printCarResult(Car car) {
+        System.out.print(car.getCarName() + " : ");
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
